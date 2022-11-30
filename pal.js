9743af0c7e694b935f5f23a1976dbae1571e619f
unknown <vikasjadhav813@gmail.com>
2022-11-30 20:23:37 +0530

    pali_complete

--- pal.js
+++ pal.js
@@ -1,1 +1,11 @@
 let a="naman"
+
+let str1="";
+    for(let i=a.length-1;i>=0;i--){
+        str1=str1+a[i];
+    }
+    if(a==str1){
+        console.log("Yes")
+    }else{
+        console.log("No")
+    }
